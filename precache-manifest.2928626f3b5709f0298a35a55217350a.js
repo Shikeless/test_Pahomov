bc7533aea86fe5c8a20f5743e5999187580dc6ef
Shikeless <Shikeless@gmail.com>
2019-12-15 18:36:53 +0300

    Updates

---
+++ precache-manifest.2928626f3b5709f0298a35a55217350a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a55060e269bdd078af1a2a06ec84ff",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "0859ae60340aa95b0702",
+    "url": "/test1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b60b9845f77b3a9ce15d",
+    "url": "/test1/static/js/2.709419e3.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/test1/static/js/2.709419e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0859ae60340aa95b0702",
+    "url": "/test1/static/js/main.990a142c.chunk.js"
+  },
+  {
+    "revision": "ecf042e213b0e2a2f756",
+    "url": "/test1/static/js/runtime-main.c1cd43dd.js"
+  },
+  {
+    "revision": "4784df4ad9c79f2c7febab7bd2349141",
+    "url": "/test1/static/media/Ellipse-1.4784df4a.png"
+  },
+  {
+    "revision": "2eff82a62810b33192597d5dcca0d915",
+    "url": "/test1/static/media/Ellipse-2.2eff82a6.png"
+  },
+  {
+    "revision": "199e976692c129017462683d268f0e97",
+    "url": "/test1/static/media/Ellipse-3.199e9766.png"
+  },
+  {
+    "revision": "6f050812fda46917adede7f1e05ba132",
+    "url": "/test1/static/media/Ellipse-4.6f050812.png"
+  },
+  {
+    "revision": "e00113dee9a331090768d7c14c8cd1ac",
+    "url": "/test1/static/media/Ellipse-5.e00113de.png"
+  },
+  {
+    "revision": "0b7b5c3594e4e8a323c821695fc18f39",
+    "url": "/test1/static/media/Ellipse-6.0b7b5c35.png"
+  },
+  {
+    "revision": "d7b0f4e03b2d0c78efdfd44e6cea04e6",
+    "url": "/test1/static/media/Ellipse-7.d7b0f4e0.png"
+  },
+  {
+    "revision": "a5fe3452d22305acc71d1f506216c87a",
+    "url": "/test1/static/media/Ellipse-8.a5fe3452.png"
+  },
+  {
+    "revision": "37fdf2671e6ad464fc5383c86e37c4e6",
+    "url": "/test1/static/media/Ellipse-9.37fdf267.png"
+  },
+  {
+    "revision": "104b7959061bf7d0f4ab2ce87fddf707",
+    "url": "/test1/static/media/Ellipse.104b7959.png"
+  }
+]);
